e54115d7959e6fe51bbe560acc9f8e13793a9f02
Faris <farissenior24@gmail.com>
2025-01-14 19:11:13 +0800

    card for admin bahagian

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -125,7 +125,7 @@
                             <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
                         </svg>Achieved
                     </h5>
-                    <h2 class="display-4">{{ $addKpis->where('peratus_pencapaian', '==', 100)->count() }}</h2>
+                    <h2 class="display-4">{{ $addKpis->where('status', 'achieved')->count() }}</h2>
                 </div>
             </div>
         </div>
@@ -137,7 +137,7 @@
                             <path d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48zm1 0v3.17c2.134.181 3 1.48 3 1.48a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351z"/>
                         </svg>Pending
                     </h5>
-                    <h2 class="display-4">{{ $addKpis->whereBetween('peratus_pencapaian', [50, 99])->count() }}</h2>
+                    <h2 class="display-4">{{ $addKpis->where('status', 'pending')->count() }}</h2>
                 </div>
             </div>
         </div>
@@ -150,7 +150,7 @@
                             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
                         </svg> Not Achieved
                     </h5>
-                    <h2 class="display-4">{{ $addKpis->whereBetween('peratus_pencapaian', [0,49])->count() }}</h2>
+                    <h2 class="display-4">{{ $addKpis->where('status', 'not achieved')->count() }}</h2>
                 </div>
             </div>
         </div>
