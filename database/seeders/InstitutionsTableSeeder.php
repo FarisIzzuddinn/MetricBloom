20ca5d66ff7f24cf77dafd2eb158c460990f8c8e
Faris <farissenior24@gmail.com>
2025-06-07 00:52:27 +0800

    institutions seeder update

--- database/seeders/InstitutionsTableSeeder.php
+++ database/seeders/InstitutionsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -13,27 +14,73 @@ class InstitutionsTableSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // List of prison institutions in Malaysia with associated state IDs
+    {   
+        $now = Carbon::now();
+        
         $institutions = [
-            ['name' => 'Penjara Kajang', 'state_id' => 12, 'description' => "Sungai Jelok, 43000 Kajang, SELANGOR DARUL EHSAN"],  // Selangor
-            ['name' => 'Penjara Sungai Buloh', 'state_id' => 12, 'description' => "Penjara Sungai Buloh, 47000 Sungai Buloh, Selangor"],  // Selangor
-            ['name' => 'Penjara Johor Bahru', 'state_id' => 1, 'description' => "KM 3, Jalan Johor Baru-Pontian, Kangkar Pulai, 81300 Skudai, Johor"],  // Johor
-            ['name' => 'Penjara Kluang', 'state_id' => 1, 'description' => "Batu 8, Jalan Mersing, 86000 Kluang, JOHOR DARUL TAKZIM"],  // Johor
-            ['name' => 'Penjara Alor Setar', 'state_id' => 2, 'description' => "Jalan Sultanah, 05350 Alor Setar, KEDAH DARUL AMAN"],  // Kedah
-            ['name' => 'Penjara Pengkalan Chepa', 'state_id' => 3, 'description' => "Jalan Maktab, 16109 Pengkalan Chepa, KELANTAN DARUL NAIM"],  // Kelantan
-            ['name' => 'Penjara Melaka', 'state_id' => 4, 'description' => "Jalan Parameswara 75000 Banda Hilir, MELAKA"],  // Melaka
-            ['name' => 'Penjara Seremban', 'state_id' => 5, 'description' => "Jalan Muthu Cumaru, 70990 Seremban, NEGERI SEMBILAN DARUL KHUSUS"],  // Negeri Sembilan
-            ['name' => 'Penjara Bentong', 'state_id' => 6, 'description' => "Kompleks Penjara Bentong, Mempaga, 28600 Karak, Bentong, PAHANG DARUL MAKMUR"],  // Pahang
-            ['name' => 'Penjara Taiping', 'state_id' => 7, 'description' => "Jalan Taming Sari, 34000 Taiping, PERAK DARUL RIDZUAN"],  // Perak
-            ['name' => 'Penjara Kangar', 'state_id' => 8, 'description' => "Jalan Kok Klang, 02500 Mata Ayer, Kangar, PERLIS"],  // Perlis
-            ['name' => 'Penjara Seberang Perai', 'state_id' => 9, 'description' => "Kompleks Penjara Seberang Perai,Jalan Kerian Kedah, 14200 Sungai Jawi, Seberang Perai Selatan, PULAU PINANG"],  // Pulau Pinang
-            ['name' => 'Penjara Kota Kinabalu', 'state_id' => 10, 'description' => "Peti Surat 11020, 88811 Kota Kinabalu, SABAH"],  // Sabah
-            ['name' => 'Penjara Kuching', 'state_id' => 11, 'description' => "Penjara Puncak Borneo KM 23, Jalan Puncak Borneo, 93250 Kuching, Sarawak"],  // Sarawak
-            ['name' => 'Penjara Marang', 'state_id' => 13, 'description' => "Mukim 21600 Kampung Kuala Tengah, 21600 Marang, Terengganu"],  // Terengganu
+           ['name' => 'IPPM', 'state_id' => '10', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PPN Perlis', 'state_id' => '8', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Pusat Koreksional Perlis', 'state_id' => '8', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PPN Kedah', 'state_id' => '2', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Alor Setar', 'state_id' => '2', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Sungai Petani', 'state_id' => '2', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Pokok Sena', 'state_id' => '2', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Akademi Koreksional Malaysia', 'state_id' => '2', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PPN Pulau Pinang', 'state_id' => '9', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Pulau Pinang', 'state_id' => '9', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Seberang Perai', 'state_id' => '9', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PPN Perak', 'state_id' => '7', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Taiping', 'state_id' => '7', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Pusat Koreksional Kamunting', 'state_id' => '7', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PLPP Taiping', 'state_id' => '7', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PPA Batu Gajah', 'state_id' => '7', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Tapah', 'state_id' => '7', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PPN Selangor & W.P Kuala Lumpur', 'state_id' => '6', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Sungai Buloh', 'state_id' => '6', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Kajang', 'state_id' => '6', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Maktab Penjara Malaysia', 'state_id' => '6', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Wanita Kajang', 'state_id' => '6', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Puncak Alam', 'state_id' => '6', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PPN Negeri Sembilan', 'state_id' => '5', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Seremban', 'state_id' => '5', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'IPD Jelebu', 'state_id' => '5', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PPN Melaka', 'state_id' => '4', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'SHG Telok Mas', 'state_id' => '4', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Dusun Dato\' Murad', 'state_id' => '4', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Sungai Udang', 'state_id' => '4', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Institut Koreksional Malaysia', 'state_id' => '4', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Muzium Penjara', 'state_id' => '4', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Pusat Koreksional Jasin', 'state_id' => '4', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PPN Johor', 'state_id' => '1', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Johor Bahru', 'state_id' => '1', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Simpang Rengam', 'state_id' => '1', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Pusat Koreksional Muar', 'state_id' => '1', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Kluang', 'state_id' => '1', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PPN Kelantan', 'state_id' => '3', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Pengkalan Chepa', 'state_id' => '3', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PPA Machang', 'state_id' => '3', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PPN Terengganu', 'state_id' => '11', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Marang', 'state_id' => '11', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Pusat Koreksional Dungun', 'state_id' => '11', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PPN Pahang', 'state_id' => '6', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Penor', 'state_id' => '6', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Bentong', 'state_id' => '6', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PPN Sarawak', 'state_id' => '13', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Puncak Borneo', 'state_id' => '13', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Miri', 'state_id' => '13', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Sibu', 'state_id' => '13', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Limbang', 'state_id' => '13', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Bintulu', 'state_id' => '13', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Sri Aman', 'state_id' => '13', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'PPN Sabah', 'state_id' => '12', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Kota Kinabalu', 'state_id' => '12', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Wanita Kota Kinabalu', 'state_id' => '12', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Tawau', 'state_id' => '12', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Sandakan', 'state_id' => '12', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'SHG Keningau', 'state_id' => '12', 'created_at' => $now, 'updated_at' => $now ],
+           ['name' => 'Penjara Labuan', 'state_id' => '12', 'created_at' => $now, 'updated_at' => $now ],
         ];
 
-        // Insert data into the institutions table
         DB::table('institutions')->insert($institutions);
     }
 }
